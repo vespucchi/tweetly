13aa15419a8eb139a325b1357c8cefe7ddd84a3e
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-09-16 18:30:46 +0200

    ADD(server): init express

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express, { type Express, type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Test');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
